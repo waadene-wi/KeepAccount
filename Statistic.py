5fbf5998b37cc87dbde33986564b38c6968b130e
waadene-wi <laili8202914@163.com>
2021-04-16 18:36:21 +0800

    lalala

--- Statistic.py
+++ Statistic.py
@@ -1,6 +1,25 @@
 from Common import *
 from DB import *
+from DictParamChecker import *
 
 class Statistic:
-    def __init__(self):
-        self.db = DB()
+    def __init__(self, logger):
+        self.db = DB.DB(logger)
+        self.logger = logger
+    
+    def test(self, args):
+        return makeReturn(Error.SUCCESS, {'hello':'world', 'from':'statistic'})
+
+    def __specialInputArgsTransfer(self, args):
+        '''成功返回轉換後的args，否則返回-1'''
+        args['begin_time'] = timestampFromString(args['begin_time'])
+        args['end_time'] = timestampFromString(args['end_time'])
+        return args
+
+    def getIncomePercentage(self, args):
+        checker = DictParamChecker()
+        checker.addParam('begin_time', 'int', True)
+        checker.addParam('end_time', 'int', True)
+        args = checker.check()
+        if type(args) == str:
+            self.logger
