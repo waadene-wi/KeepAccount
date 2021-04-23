89c568e35c272380fd8a4f04fa10b31ec3f3ccd4
waadene-wi <laili8202914@163.com>
2021-04-23 22:45:59 +0800

    hahaha

--- ReloadAllData.py
+++ ReloadAllData.py
@@ -1,4 +1,3 @@
-import json
 import Account
 import Category
 import Record
@@ -6,12 +5,19 @@ import Statistic
 import Budget
 import Currency
 import Common
+import json
 import logging
+import sqlite3
+import os
 
-BACKUP_FILE_NAME = 'commit.backup'
+path = os.path.abspath(__file__)
+dirname, useless = os.path.split(path)
+ROOT_DIR = dirname + '/'
+DB_FILE_NAME = ROOT_DIR + 'ka.db'
+BACKUP_FILE_NAME = ROOT_DIR + 'commit.backup'
+
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger('ReloadAllData')
-
 
 service_list = {}
 service_list['account'] = Account.Account(logger)
@@ -21,6 +27,25 @@ service_list['statistic'] = Statistic.Statistic(logger)
 service_list['budget'] = Budget.Budget(logger)
 service_list['currency'] = Currency.Currency(logger)
 
+# 删除数据库文件
+os.remove(DB_FILE_NAME)
+
+# 读取创建数据库SQL文件内容
+f = open(ROOT_DIR + 'sql/create.sql')
+create_sql_content = f.read()
+f.close()
+f = open(ROOT_DIR + 'sql/add_basic_data.sql')
+add_basic_data_sql_content = f.read()
+f.close()
+
+# 重新创建数据库
+connect = sqlite3.connect(DB_FILE_NAME)
+connect.executescript(create_sql_content)
+connect.executescript(add_basic_data_sql_content)
+connect.commit()
+connect.close()
+
+# 读取backup文件恢复数据
 f = open(BACKUP_FILE_NAME)
 while True:
     line = f.readline()
@@ -36,3 +61,4 @@ while True:
     if ret['errno'] != Common.Error.SUCCESS:
         raise ValueError('execute failed! line: ' + line)
 f.close()
+
